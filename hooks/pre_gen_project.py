dd39b824e114193b2509ff8b766d257ade5c26c1
Roni Dover <roni.dover@gmail.com>
2016-07-13 16:10:38 +0300

    hook

--- hooks/pre_gen_project.py
+++ hooks/pre_gen_project.py
@@ -2,4 +2,4 @@ from shutil import copyfile
 import os
 
 #print os.listdir(os.getcwd())
-copyfile('{{cookiecutter.app_name}}' + "./spec/datamodel.xml", "./template/{{cookiecutter.project_slug}}/datamodel.xml")
+copyfile('{{cookiecutter.project_name}}' + "./spec/datamodel.xml", "./template/{{cookiecutter.project_slug}}/datamodel.xml")
